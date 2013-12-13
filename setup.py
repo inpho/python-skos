55ea76cce47c60d6b7673fc27cc8c93366b2e3cf
Jaimie Murdock <jaimie.murdock@gmail.com>
2013-12-13 16:03:57 -0500

    adding setup file stuff

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Command
+#from distutils.core import setup, Command
+from setuptools import setup
 from skos import __version__
-
+'''
 class TestCommand(Command):
     """
     Custom distutils command for running the test suite
@@ -22,14 +23,15 @@ class TestCommand(Command):
         test_dir = os.path.join(os.path.dirname(__file__), 'test')
         package_suite = unittest.TestLoader().discover(test_dir)
         unittest.TextTestRunner(verbosity=2).run(package_suite)
-
+'''
 setup(name='python-skos',
       version=__version__,
       description='A basic implementation of some core elements of the SKOS object model',
-      author='Homme Zwaagstra',
+      author='Jaimie Murdock',
       author_email='hrz@geodata.soton.ac.uk',
-      url='http://github.com/geo-data/python-skos',
+      url='http://github.com/inpho/python-skos',
       license='BSD',
       py_modules=['skos'],
-      cmdclass = { 'test': TestCommand }
+  #    cmdclass = { 'test': TestCommand },
+      install_requires =["RDFLib==2.4.2", "SQLAlchemy>=0.7.5", "iso8601plus"]
      )
